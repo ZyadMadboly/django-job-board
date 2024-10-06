a19f00e479e63e53eb870f727351fef64e558018
ZyadMadboly <zeiadmadboly@gmail.com>
2024-10-06 15:01:35 +0300

    add job views and url

--- job/admin.py
+++ job/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Job # import model job
+from .models import Job , Category # import models
 
 admin.site.register(Job) # push model job to admin panel
+admin.site.register(Category)
